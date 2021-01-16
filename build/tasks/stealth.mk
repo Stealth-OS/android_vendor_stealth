84e8794be83d194f23dab0b87cb4d5c8fa72e878
Stealth1226 <rishawniyer06@gmail.com>
2021-01-16 16:00:53 +0530

    Updated Completion Message

--- build/tasks/stealth.mk
+++ build/tasks/stealth.mk
@@ -15,10 +15,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-PROD_VERSION := StealthOS-1.0
-TARGET_FILES_PACKAGE := $(PRODUCT_OUT)/$(PROD_VERSION).zip
+
+#
+# Build system colors
+#
+# PFX: Prefix "target C++:" in yellow
+# INS: Module "Install:" output color (cyan for ics)
+
+ifneq ($(BUILD_WITH_COLORS),0)
+  CL_PFX="\033[33m"
+  CL_INS="\033[36m"
+  CL_RED="\033[31m"
+  CL_GRN="\033[32m"
+  CL_YLW="\033[33m"
+  CL_BLU="\033[34m"
+  CL_MAG="\033[35m"
+  CL_CYN="\033[36m"
+  CL_RST="\033[0m"
+endif
+
+PLATFORM_STEALTH_RELEASE := StealthOS 1.0
+STEALTH_VERSION := $(PLATFORM_STEALTH_RELEASE).$(shell date +%m%d%H%M)
+
+TARGET_FILES_PACKAGE := $(PRODUCT_OUT)/$(STEALTH_VERSION).zip
 
 .PHONY: stealth
 stealth: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(TARGET_FILES_PACKAGE)
-	@echo "Package Complete: $(TARGET_FILES_PACKAGE)" >&2
+        @echo "Build Success! , Find the package at: $(TARGET_FILES_PACKAGE)" >&2
+
